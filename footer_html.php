3e70e5d43daa4a6d499d1679572d5cf3f6c4d4f3
Vinícius Alves Hax <viniciushax@gmail.com>
2022-12-23 00:54:59 -0300

    Adicionando link para as ações de tarefas

--- footer_html.php
+++ footer_html.php
@@ -1,8 +1,11 @@
 <h2>Ações</h2>
 <ul>
-<li><a href="add_task.php">Adicionar tarefa</a></li>
 <li><a href="add_objective.php">Adicionar objetivo</a></li>
+<li><a href="add_task.php">Adicionar tarefa</a></li>
+
 <li><a href="list_objectives.php">Listar objetivos</a></li>
+<li><a href="index.php">Listar tarefas</a></li>
+
 </ul>
 
 </body>
