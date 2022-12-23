c7a388c28bce191aed7ccfe5a7f0145842398018
Vinícius Alves Hax <viniciushax@gmail.com>
2022-12-22 23:16:59 -0300

    Adicionando formulário de criação de objetivo

---
+++ add_objective.php
@@ -0,0 +1,9 @@
+
+<h2>Adicionar objetivo</h2>
+
+<form action="add_objective2.php" method="post">
+
+<input type="text" placeholder="Descreva o objetivo" />
+<input type="button" value="Cadastrar" />
+
+</form>
