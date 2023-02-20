ff50f9c046b5c7336b2f2bf451e502b3b40b3e53
Vinícius Alves Hax <viniciushax@gmail.com>
2023-02-20 16:20:43 -0300

    Colocando followup = FALSE nas tarefas marcadas como feitas

--- done_task.php
+++ done_task.php
@@ -10,7 +10,7 @@ $id = $_GET["id"];
 
 $today = date("Y-m-d");
 
-$sql = "UPDATE tasks SET done = TRUE, done_date = '$today' WHERE id = $id"; 
+$sql = "UPDATE tasks SET followup=FALSE, done = TRUE, done_date = '$today' WHERE id = $id"; 
 $result = query_or_die_trying($sql);
 
 if ($result) {
