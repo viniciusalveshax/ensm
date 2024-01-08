eac3486f5a7824f0499f771d9fe439a836abc183
Vinícius Alves Hax <viniciushax@gmail.com>
2024-01-08 02:30:50 -0300

    Mostrando também as metas associadas com cada objetivo

--- list_objectives.php
+++ list_objectives.php
@@ -5,16 +5,19 @@ require "header_html.php";
 
 echo "<ul>";
 
-$sql = "SELECT * from objectives WHERE hide = 0 ORDER BY context_id"; 
+//$sql = "SELECT * from objectives WHERE hide = 0 ORDER BY importance"; 
+//SELECT * FROM objectives JOIN goals WHERE goals.objective_id = objectives.id 
+$sql = "SELECT * FROM objectives JOIN goals WHERE goals.objective_id = objectives.id ORDER BY objectives.importance, goals.objective_id";
+$sql = "SELECT importance, objectives.description AS objective_description, goals.description AS goal_description FROM objectives JOIN goals WHERE goals.objective_id = objectives.id ORDER BY objectives.importance, goals.objective_id";
 
-echo $sql;
+//echo $sql;
 
 $result = query_or_die_trying($sql);
 
 if ($result) {
 
 	while ($line = mysqli_fetch_assoc($result)) {
-		echo "<li>" . $line['description'] . " Contexto: " . context_name($line['context_id']) . "</li>";
+		echo "<li>Importância: " . $line["importance"] . ", Objetivo: " . $line['objective_description'] . ", Meta: " . $line['goal_description'] . "</li>";
 	}
 
 	}
